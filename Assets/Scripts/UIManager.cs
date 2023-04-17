ec9f1ebf08edd5e2ea2bd8e675ab2edd69cb8558
Sofian.TF <psg93100@gmail.com>
2023-04-17 18:42:20 +0900

    Add title screen

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _startText;
+    [SerializeField] private float _flickerInterval = 0.25f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        StartCoroutine(FlickerText(_startText, _flickerInterval));
+    }
+
+    IEnumerator FlickerText(TextMeshProUGUI textToFlicker, float interval)
+    {
+        while (true)
+        {
+            textToFlicker.color = new Color(1f, 1f, 1f, 0.25f);
+            yield return new WaitForSeconds(interval);
+            textToFlicker.color = new Color(1f, 1f, 1f, 1f);
+            yield return new WaitForSeconds(interval);
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && SceneManager.GetActiveScene().name == "TitleScreen")
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
